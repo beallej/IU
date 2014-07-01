69b9155f3ef06f0ca792063ae3dfbcf023091d8b
beallej <beallej@carleton.edu>
2014-07-01 17:27:53 -0400

    end of tuesday

--- tcheck_modified.rkt
+++ tcheck_modified.rkt
@@ -2,24 +2,24 @@
 (require "pmatch.rkt")
 (provide typecheck)
 (define consistent?
-  (lambda (T1 T2)
+  (lambda (FT1 FT2)
    
-    (pmatch `(,T1 ,T2)
-            (`(,T1 dyn) #t)
-            (`(dyn ,T2) #t)
+    (pmatch `(,FT1 ,FT2)
+            (`(,FT1 dyn) #t)
+            (`(dyn ,FT2) #t)
             (`(int int) #t)
             (`(bool bool) #t)
-            (`((-> ,T11 ,T12) (-> ,T21 ,T22)) (and (consistent? T12 T22) (consistent? T11 T21)))
+            (`((-> ,FT11 ,FT12) (-> ,FT21 ,FT22)) (and (consistent? FT12 FT22) (consistent? FT11 FT21)))
             (`other #f))))
 (define meet
-  (lambda (T1 T2)
+  (lambda (TT1 TT2)
     
-    (pmatch `(,T1 ,T2)
-            (`(,T1 dyn) T1)
-            (`(dyn ,T2) T2)
-            (`(int int) 'int)
-            (`(bool bool) 'bool)
-            (`((-> ,T11 ,T12) (-> ,T21 ,T22)) (-> (meet T11 T21) (meet T12 T22)))
+    (pmatch `(,TT1 ,TT2)
+            (`(,TT1 dyn) TT1)
+            (`(dyn ,TT2) TT2)
+            (`(int int) `int)
+            (`(bool bool) `bool)
+            (`((-> ,TT11 ,TT12) (-> ,TT21 ,TT22)) (-> (meet TT11 TT21) (meet TT12 TT22)))
             (`other (error 'meet "types are not consistent")))))
 (define cast
   (lambda (l e T1 T2)
@@ -40,11 +40,11 @@
 (define typeof
   (lambda (k)
     (pmatch k
-            (`,n (guard (integer? n)) 'int)
-            (`,b (guard (boolean? b)) 'bool)
-            (`inc '(-> int int))
-            (`dec '(-> int int))
-            (`zero? '(-> int bool)))))
+            (`,n (guard (integer? n)) `int)
+            (`,b (guard (boolean? b)) `bool)
+            (`inc `(-> int int))
+            (`dec `(-> int int))
+            (`zero? `(-> int bool)))))
 
 (define mk-cast
   (lambda (l e T1 T2)
@@ -64,7 +64,7 @@
                      (`((,new-cnd ,cnd-T) (,new-thn ,thn-T) (,new-els ,els-T)) (cond
                                                                                  ((and (consistent? thn-T els-T) (consistent? cnd-T 'bool))
                                                                                   (let ((if-T (meet thn-T els-T)))
-                                                                                    `((if ,(mk-cast l new-cnd cnd-T 'bool) ,(mk-cast l new-thn thn-T if-T) ,(mk-cast l new-thn els-T if-T)) ,if-T)))
+                                                                                    `((if ,(mk-cast l new-cnd cnd-T `bool) ,(mk-cast l new-thn thn-T if-T) ,(mk-cast l new-thn els-T if-T)) ,if-T)))
                                                                                  (else (error 'typecheck "ill-typed expression"))))))
             (`,x (guard (symbol? x)) `(,x ,(cdr (assq x env))))
             (`(lambda (,x) ,e1) (typecheck env `(lambda (,x : dyn) ,e1)))
@@ -104,7 +104,7 @@
                      (`((,new-cnd ,cnd-T) (,new-thn ,thn-T) (,new-els ,els-T)) (cond
                                                                                  ((and (consistent? thn-T els-T) (consistent? cnd-T 'bool))
                                                                                   (let ((if-T (meet thn-T els-T)))
-                                                                                    `((if ,(mk-cast l new-cnd cnd-T 'bool) ,(mk-cast l new-thn thn-T if-T) ,(mk-cast l new-thn els-T if-T)) ,if-T)))
+                                                                                    `((if ,(mk-cast l new-cnd cnd-T `bool) ,(mk-cast l new-thn thn-T if-T) ,(mk-cast l new-thn els-T if-T)) ,if-T)))
                                                                                  (else (error 'typecheck "ill-typed expression"))))))
             (`,x (guard (symbol? x)) `(,x ,(car (cdr (cdr (assq x env))))))
             (`(lambda (,x ,id) ,e1) (typecheck env `(lambda (,x ,id : dyn) ,e1)))
